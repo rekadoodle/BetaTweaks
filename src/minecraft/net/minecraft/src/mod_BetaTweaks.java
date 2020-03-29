7ea9ebadb2051516962ce083ef6925db259303e6
JosephBarker <rekrabmail@gmail.com>
2020-03-29 00:18:29 +0000

    Formatting

--- src/minecraft/net/minecraft/src/mod_BetaTweaks.java
+++ src/minecraft/net/minecraft/src/mod_BetaTweaks.java
@@ -8,81 +8,80 @@ public class mod_BetaTweaks extends BaseMod {
 	public mod_BetaTweaks() {
 		BetaTweaks.INSTANCE.init(this);
 	}
-	
+
 	@Override
 	public String Version() {
 		return ModInfo.VERSION;
 	}
-	
+
 	@Override
-	public void KeyboardEvent(KeyBinding keybinding)
-    {
+	public void KeyboardEvent(KeyBinding keybinding) {
 		BetaTweaks.INSTANCE.keyboardEvent(keybinding);
-    }
+	}
 
 	@Override
 	public void ModsLoaded() {
 		BetaTweaks.INSTANCE.modsLoaded();
 	}
-	
+
 	@Override
 	public boolean OnTickInGUI(Minecraft mc, GuiScreen guiscreen) {
 		BetaTweaks.INSTANCE.onTickInGUI(mc, guiscreen);
 		return true;
 	}
 
-    @Override
+	@Override
 	public boolean OnTickInGame(Minecraft mc) {
 		BetaTweaks.INSTANCE.onTickInGame(mc);
 		return true;
 	}
-	
-	//These methods are used to avoid reflection in classes from the betatweaks package.
+
+	// These methods are used to avoid reflection in classes from the betatweaks package.
 	public static void drawRect(int i, int j, int k, int l, int colour) {
 		Graphics.gui.drawRect(i, j, k, l, colour);
 	}
-	
+
 	public static java.nio.FloatBuffer fogColorBuffer(EntityRenderer entityrenderer) {
 		return entityrenderer.fogColorBuffer;
 	}
-	
+
 	public static float fogColorRed(EntityRenderer entityrenderer) {
 		return entityrenderer.fogColorRed;
 	}
-	
+
 	public static float fogColorGreen(EntityRenderer entityrenderer) {
 		return entityrenderer.fogColorGreen;
 	}
-	
+
 	public static float fogColorBlue(EntityRenderer entityrenderer) {
 		return entityrenderer.fogColorBlue;
 	}
-	
+
 	public static void setFallDistance(Entity entity, float fallDistance) {
 		entity.fallDistance = fallDistance;
 	}
-	
+
 	public static int xSize(GuiContainer container) {
 		return container.xSize;
 	}
-	
+
 	public static int ySize(GuiContainer container) {
 		return container.ySize;
 	}
-	
+
 	public static java.util.List<?> controlList(GuiScreen guiscreen) {
 		return guiscreen.controlList;
 	}
 
-	//Info for mine_diver's mod menu
+	// Info for mine_diver's mod menu
 	public String Name() {
 		return ModInfo.MODMENU_NAME;
 	}
-	
+
 	public String Description() {
 		return ModInfo.MODMENU_DESC;
 	}
-	
+
 	public String Icon() {
 		return ModInfo.MODMENU_ICON;
 	}
